c7b0cf0a1cde4ba4176befde04ce7af7013bfcc4
monthalcantara <montival_junior@yahoo.com.br>
2022-02-25 19:46:51 -0300

    resolucao da unicidade login usuario

--- Desafio-02-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/repository/UsuarioEntityRepository.java
+++ Desafio-02-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/repository/UsuarioEntityRepository.java
@@ -1,8 +1,7 @@
 package io.github.monthalcantara.mercadolivre.repository;
 
 import io.github.monthalcantara.mercadolivre.entity.UsuarioEntity;
-import io.github.monthalcantara.mercadolivre.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsuarioRepository extends CrudRepository<UsuarioEntity, Long> {
+public interface UsuarioEntityRepository extends CrudRepository<UsuarioEntity, Long> {
 }
